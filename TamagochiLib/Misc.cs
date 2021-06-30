9d2e4504b60ced6a8db643712cf7d42c2cf23b6f
femperox <ifilchukova1@gmail.com>
2021-06-30 20:48:46 +0300

    Класс для вспомогательных решений

---
+++ TamagochiLib/Misc.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace TamagochiLib
+{
+    class Misc
+    {
+        // Возвращает имя объекта
+        public static string GetObjectName(object o)
+        {
+            string type = o.GetType().ToString();
+            Regex regex = new Regex(@"^(\w*)\.");
+            return regex.Replace(type, "");
+
+        }
+
+        // кидает исключения
+        public static void ThrowEX(string e) => throw new Exception(e);
+    }
+}
